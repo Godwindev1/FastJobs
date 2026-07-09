92f8314af83eb3316d1ee1f8d89bcbbb91031104
Godwin Oluowho <gamedev740@gmail.com>
2026-07-09 13:51:50 +0100

    Added: MSSQL Test Containers implementation for Repository Tests

---
+++ Tests/Integration/Repositories/DB-Providers/MSSQLFixture.cs
@@ -0,0 +1,21 @@
+using Testcontainers.MsSql;
+
+public class MsSqlFixture : IAsyncLifetime
+{
+    private readonly MsSqlContainer _container = new MsSqlBuilder()
+        .WithImage("mcr.microsoft.com/mssql/server:2022-latest")
+        .WithPassword("FastJobs-Integration-Test-Pa$$w0rd2026")
+        .Build();
+
+    public string ConnectionString => _container.GetConnectionString();
+
+    public async Task InitializeAsync()
+    {
+        await _container.StartAsync();
+    }
+
+    public async Task DisposeAsync()
+    {
+        await _container.DisposeAsync();
+    }
+}
--- Tests/Integration/Repositories/FastjobsHostFixture.cs
+++ Tests/Integration/Repositories/FastjobsHostFixture.cs
@@ -1,16 +1,26 @@
 using FastJobs;
 using FastJobs.Persistence;
 using Microsoft.Extensions.Hosting;
+using DotNetEnv;
 
 public class FastJobsHostFixture : IAsyncLifetime
 {
     public IHost Host { get; private set; } = default!;
     public IJobRepository Repository { get; private set; } = default!;
 
-    string connectionstring = "Server=(localdb)\\FastjobsDBServer;Database=FastJobs;Trusted_Connection=True;MultipleActiveResultSets=true";
+    private readonly MsSqlFixture _msSqlFixture = new();
+
+    //string connectionstring = "Server=(localdb)\\FastjobsDBServer;Database=FastJobs;Trusted_Connection=True;MultipleActiveResultSets=true";
 
     public async Task InitializeAsync()
     {
+        Env.Load();
+        Console.WriteLine($"DOCKER_HOST resolved to: {Environment.GetEnvironmentVariable("DOCKER_HOST")}");
+
+        await _msSqlFixture.InitializeAsync();
+
+        string connectionstring = _msSqlFixture.ConnectionString;
+
         var builder = Microsoft.Extensions.Hosting.Host.CreateApplicationBuilder();
         builder.Services.AddFastJobs(o => o.WorkerCount = 1, new FastJobMSSQLDependencies(x =>{  x.ConnectionString = connectionstring; x.SchemaName = "FastjobsDB"; } ));
         Host = builder.Build();
