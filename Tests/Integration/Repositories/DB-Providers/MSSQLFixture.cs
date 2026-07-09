bfe02e5aa154dd9e597bb3a7afaffeb9e314c100
Godwin Oluowho <gamedev740@gmail.com>
2026-07-09 17:46:54 +0100

    Update: Use Tcp for remote Docker Host instead

--- Tests/Integration/Repositories/DB-Providers/MSSQLFixture.cs
+++ Tests/Integration/Repositories/DB-Providers/MSSQLFixture.cs
@@ -2,15 +2,17 @@ using Testcontainers.MsSql;
 
 public class MsSqlFixture : IAsyncLifetime
 {
-    private readonly MsSqlContainer _container = new MsSqlBuilder()
-        .WithImage("mcr.microsoft.com/mssql/server:2022-latest")
-        .WithPassword("FastJobs-Integration-Test-Pa$$w0rd2026")
-        .Build();
+    private MsSqlContainer _container ;
 
     public string ConnectionString => _container.GetConnectionString();
 
     public async Task InitializeAsync()
     {
+        _container = new MsSqlBuilder()
+        .WithImage("mcr.microsoft.com/mssql/server:2022-latest")
+        .WithPassword("FastJobs-Integration-Test-Pa$$w0rd2026")
+        .Build();
+
         await _container.StartAsync();
     }
 
