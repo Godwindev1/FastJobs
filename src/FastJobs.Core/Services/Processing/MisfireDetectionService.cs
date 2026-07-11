c1900ed0d1fa71636ef752f09947625fc0708f9a
Godwin Oluowho <gamedev740@gmail.com>
2026-07-11 17:11:55 +0100

    fix: Allows Configuring the initial startup delay for MisfireDetection Service through fastjobOptions class

--- src/FastJobs.Core/Services/Processing/MisfireDetectionService.cs
+++ src/FastJobs.Core/Services/Processing/MisfireDetectionService.cs
@@ -4,17 +4,19 @@ public class MisfireDetectorService : BackgroundService
 {
     private readonly RecurringMisfireDetector _misfireDetector;
     private readonly TimeSpan _interval = TimeSpan.FromMinutes(1);
+    private readonly TimeSpan _MisfireDetectorStartupDelay = TimeSpan.FromMinutes(1);
 
     public MisfireDetectorService(RecurringMisfireDetector misfireDetector, FastJobsOptions options)
     {
         _misfireDetector = misfireDetector;
+        _MisfireDetectorStartupDelay = options.MisfireDetectorStartupDelay;
         _interval = options.MisfireDetectorInterval;
     }
 
     protected override async Task ExecuteAsync(CancellationToken ct)
     {
         //initial Delay To make Sure all other Systems Are Running Before this Starts 
-         await Task.Delay(TimeSpan.FromSeconds(15), ct);
+         await Task.Delay(_MisfireDetectorStartupDelay, ct);
 
         using var timer = new PeriodicTimer(_interval);
 
